6df79e26f363873dc5d584c66aca4feb5d61fe98
Rob-n-Dude <m.r.stickcw@yandex.ru>
2021-02-26 15:04:48 +0300

    fix: reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function reverse (n) {
+    if(n < 0)
+    {
+        n*=-1
+    }
+    n = n.toString()
+    result = ""
+    for (let i = n.length-1; i >= 0; i--){
+        result += n[i]
+    }
+    return result
   
 }
